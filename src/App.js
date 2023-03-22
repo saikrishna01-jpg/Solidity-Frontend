cae941158452b44a591e1f26fc40d2b7497c0847
saikrishna01-jpg <85012804+saikrishna01-jpg@users.noreply.github.com>
2023-03-22 15:23:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect } from "react";
+import Web3 from "web3"
+import contractAbi from "./Contract.json"
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [storedValue, setStoredValue] = useState(0);
+  
+  const web3 = new Web3("http://localhost:7545")
+
+  const address = '0xEfec28b6A2Ef58e4d4F9b29937382c78e5f980ed';
+  const myContract = new web3.eth.Contract(contractAbi, address)
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    await myContract.methods.setValue(value).send(
+      { from: '0x3a48FA9A4d1448b2f210C9Dc1Bf38a688A20Eb86'}
+    );
+    const val = await myContract.methods.getValue().call();
+    setStoredValue(val)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>SimpleContract Example</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          New Value:
+          <input type="number" value={value} onChange={(event) => setValue(event.target.value)} />
+        </label>
+        <button type="submit">Set Value</button>
+      </form>
+      <div>
+        Stored Value: <span>{storedValue} </span> 
+      </div>
     </div>
   );
 }
